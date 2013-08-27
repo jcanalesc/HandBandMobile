d87f523cb7c9c3fde8895eb50a79ef939cfccfc9
Juan Canales <juancanalescpp@gmail.com>
2013-08-27 13:33:22 -0400

    Agregados graficos con Flot y funcionalidades basicas de UI Indicador de pagina, pulido de interfaz Aun no tiene funcionalidad, trabajando con archivo dummy

--- platforms/android/assets/www/js/dummy-data.js
+++ platforms/android/assets/www/js/dummy-data.js
@@ -2,26 +2,45 @@ var Responses = {
 	genteActual: {
 		cortesia: 0, 
 		general: 0,
-		total: 0
+		total: 0,
+		datos: [
+			[1, 10],
+			[2, 9],
+			[3, 8],
+			[4, 5],
+			[5, 9],
+			[6, 12],
+			[7, 17],
+			[8, 15]
+		]
 	},
 	reportes: {
 		dia: [
 			[19,3,2013],
-			[18,3,2013]
+			[18,3,2013],
+			[17,3,2013],
+			[16,3,2013],
+			[15,2,2013],
+			[14,2,2013],
+			[13,2,2013]
 		],
 		mes: [
 			[5,2013],
 			[4,2013],
-			[3,2013]
+			[3,2013],
+			[2,2013],
+			[1,2013]
 		],
-		ano: [2013,2012,2011]
+		ano: [2013,2012,2011,2010,2009,2008]
 	},
 	reporte: {
-		datos_grafico: 
-		{
-			x: [],
-			y: [],
-		},
+		datos_grafico: [
+			[1,1],
+			[2,3],
+			[3,2],
+			[4,3],
+			[5,4]
+		],
 		stats: [
 			{
 				label: "Mejor Dia",
@@ -35,8 +54,3 @@ var Responses = {
 	}
 };
 
-for (var i = 0; i < 60; i++)
-{
-	Responses.reporte.datos_grafico.x.push(i);
-	Responses.reporte.datos_grafico.y.push(parseInt(Math.random()*100));
-}
