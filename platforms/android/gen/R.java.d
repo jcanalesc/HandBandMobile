b2f98c2b9928b3666117887b5c9cf9d8b596c1f8
Juan Canales <juancanalescpp@gmail.com>
2013-10-23 13:03:20 -0300

    Arreglados problemas de swiping y orientacion en iOS

--- platforms/android/gen/R.java.d
+++ platforms/android/gen/R.java.d
@@ -1,14 +1,23 @@
-/home/jcanales/github/HandBandMobile/platforms/android/gen/com/phonegap/handband/R.java \
- : /home/jcanales/github/HandBandMobile/platforms/android/res/xml/config.xml \
-/home/jcanales/github/HandBandMobile/platforms/android/res/drawable-xhdpi/ic_launcher.png \
-/home/jcanales/github/HandBandMobile/platforms/android/res/drawable-xhdpi/icon.png \
-/home/jcanales/github/HandBandMobile/platforms/android/res/drawable-hdpi/ic_launcher.png \
-/home/jcanales/github/HandBandMobile/platforms/android/res/drawable-hdpi/icon.png \
-/home/jcanales/github/HandBandMobile/platforms/android/res/drawable/icon.png \
-/home/jcanales/github/HandBandMobile/platforms/android/res/drawable-ldpi/ic_launcher.png \
-/home/jcanales/github/HandBandMobile/platforms/android/res/drawable-ldpi/icon.png \
-/home/jcanales/github/HandBandMobile/platforms/android/res/values/strings.xml \
-/home/jcanales/github/HandBandMobile/platforms/android/res/layout/main.xml \
-/home/jcanales/github/HandBandMobile/platforms/android/res/drawable-mdpi/ic_launcher.png \
-/home/jcanales/github/HandBandMobile/platforms/android/res/drawable-mdpi/icon.png \
-/home/jcanales/github/HandBandMobile/platforms/android/bin/AndroidManifest.xml \
+/home/jcanales/handband/HandBandMobile/platforms/android/gen/com/phonegap/handband/R.java \
+ : /home/jcanales/handband/HandBandMobile/platforms/android/res/drawable-xhdpi/icon.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/res/drawable-xhdpi/ic_launcher.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/res/drawable/icon.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/res/drawable-hdpi/icon.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/res/drawable-hdpi/ic_launcher.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/res/xml/config.xml \
+/home/jcanales/handband/HandBandMobile/platforms/android/res/drawable-ldpi/icon.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/res/drawable-ldpi/ic_launcher.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/res/layout/main.xml \
+/home/jcanales/handband/HandBandMobile/platforms/android/res/drawable-mdpi/icon.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/res/drawable-mdpi/ic_launcher.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/res/values/strings.xml \
+/home/jcanales/handband/HandBandMobile/platforms/android/bin/res/drawable-xhdpi/icon.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/bin/res/drawable/icon.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/bin/res/drawable-hdpi/icon.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/bin/res/drawable-hdpi/ic_launcher.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/bin/res/drawable-ldpi/icon.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/bin/res/drawable-ldpi/ic_launcher.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/bin/res/drawable-mdpi/icon.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/bin/res/drawable-mdpi/ic_launcher.png \
+/home/jcanales/handband/HandBandMobile/platforms/android/bin/AndroidManifest.xml \
